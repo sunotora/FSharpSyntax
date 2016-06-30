76de173639981b249ac0064669d8fa61e794b528
sunotora <sunotora@gmail.com>
2016-07-01 03:21:58 +0900

    add Lambda.fs

---
+++ Lambda.fs
@@ -0,0 +1,20 @@
+﻿namespace Lambda
+module Lambda =
+
+  // func1, func2は同じ引数の関数
+  let func1 = fun x -> x * x
+  let func2 x = x * x
+  
+  // func3, func4は同じ引数の関数
+  let func3 = fun x -> fun y -> fun z -> x + y + z
+  let func4 x y z = x + y + z
+
+  // func3はfunc5のように省略できる
+  let func5 = fun x y z -> x + y + z
+
+  // func3をインデント付与するとfunc6となる
+  let func6 =
+    fun x ->
+      fun y ->
+        fun z ->
+          x + y + z 
--- Run.fsx
+++ Run.fsx
@@ -1,5 +1,7 @@
 ﻿#load "Namespace.fs"
+#load "Lambda.fs"
 
 module Run =
   let namespaceRun = Namespace1.Child.Module1_1.hoge
 
+  let lambdaRun = Lambda.Lambda.func1 5
